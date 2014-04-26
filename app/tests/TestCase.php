79c3c9ae2082ad8f0122390d03b2aaf39a84bd54
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-04-26 15:04:59 +0200

    Fix tests and add composer.lock to repo

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -16,6 +16,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
         $app = require __DIR__ . '/../../bootstrap/start.php';
         \App::setLocale('en'); // We're setting default locale on test env
         $app['config']['gzero.multilang.detected'] = TRUE;
+        Artisan::call('doctrine:schema:create'); // Generate schema
         return $app;
     }
 
