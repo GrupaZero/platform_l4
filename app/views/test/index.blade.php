66155afbedf10fdb2c4212142a9ef2a8eb8d99b6
Przemyslaw Skierniewski <skierniewski.przemyslaw@gmail.com>
2015-02-22 15:23:55 +0100

    Frontend test route view

--- app/views/test/index.blade.php
+++ app/views/test/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.default')
 
 @section('content')
-    <table class="table table-bordered">
+    <table class="table table-bordered text-center">
         <thead>
         <tr>
-            <td>@lang('common.title')</td>
+            <td class="text-left">@lang('common.title')</td>
             <td>@lang('common.status')</td>
             <td>@lang('common.order')</td>
             <td>@lang('common.author')</td>
-            <td>@lang('common.path')</td>
+            <td class="text-left">@lang('common.path')</td>
             <td>@lang('common.type')</td>
         </tr>
         </thead>
--- app/views/test/treeNode.blade.php
+++ app/views/test/treeNode.blade.php
@@ -1,26 +1,26 @@
-<tr>
-    <td {{$content->parentId ? 'style="padding-left: ' . $content->level * 2 . '0px"' : ''}}>
+<tr{{ $content->parentId ? ' style="background-color: rgba(182, 182, 182, 0.' . $content->level . ');"' : '' }}>
+    <td class="text-left"{{ $content->parentId ? ' style="padding-left: ' . $content->level * 2 . '0px;"' : '' }}>
         <?php $activeTranslation = $content->translation($lang->code); ?>
         <?php $activeRoute = $content->routeTranslation($lang->code); ?>
         @if($activeTranslation)
-            <a href="{{ $activeRoute->url }}">{{ $activeTranslation->title }}</a>
+            <a href="{{ $activeRoute->url }}"> {{ $activeTranslation->title }}</a>
         @else
             <p>@lang('common.noTranslation')</p>
         @endif
     </td>
-    <td class="text-center">
+    <td>
         <i class="glyphicon glyphicon-{{ $content->isActive ? 'ok' : 'remove' }}-circle"></i>
     </td>
-    <td class="text-center">
+    <td>
         {{ $content->weight }}
     </td>
-    <td class="text-center">
+    <td>
         {{ $content->authorName() }}
     </td>
-    <td>
+    <td class="text-left">
         {{ $content->path }}
     </td>
-    <td class="text-center">
+    <td>
         {{ $content->type }}
     </td>
 </tr>
