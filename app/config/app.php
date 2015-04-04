8ec383e6af94feb3f700c186cee0db9c145c5eb6
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2015-04-04 09:15:56 +0200

    Forgot to remove

--- app/config/app.php
+++ app/config/app.php
@@ -122,8 +122,7 @@ return array(
         'Gzero\Core\ServiceProvider',
         'Gzero\Admin\ServiceProvider',
         'Gzero\Api\ServiceProvider',
-        'Gzero\Social\ServiceProvider',
-        'Gzero\Billin\ServiceProvider',
+        'Gzero\Social\ServiceProvider'
     ),
     /*
     |--------------------------------------------------------------------------
